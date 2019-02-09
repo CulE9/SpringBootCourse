c5241b17a150527da5e281411ede8fd9c15ad076
CulE <undertaker007@onet.pl>
2019-02-09 16:09:59 +0100

    Added userList method

--- src/main/java/pl/cule/springbootcourse/UserController.java
+++ src/main/java/pl/cule/springbootcourse/UserController.java
@@ -1,11 +1,17 @@
 package pl.cule.springbootcourse;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+
+    private List<User> users = new ArrayList<>();
 
     @ResponseStatus(value = HttpStatus.CREATED)
     @RequestMapping(value = "/registry", method = RequestMethod.POST)
@@ -25,6 +31,13 @@ public class UserController {
 
     @RequestMapping(value = "/add", method = RequestMethod.PUT)
     public User add(@RequestBody User user) {
+        users.add(user);
+        System.out.println("Utworzono: " + user.getFirstName() + " " + user.getLastName());
         return user;
+    }
+
+    @RequestMapping(value = "/userList", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
